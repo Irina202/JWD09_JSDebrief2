7d1b31ec02ab1494096325739ae16e12f27111b4
Irina202 <iracherepanova@gmail.com>
2022-11-13 19:42:23 +1100

    Scope done

---
+++ Scope/index.js
@@ -0,0 +1,28 @@
+//Scope
+
+ var customerName = 'iryna';
+
+ function upperCaseCustomerName() {
+   return  customerName.toUpperCase();
+ }
+
+ console.log(upperCaseCustomerName());
+
+ function setBestCustomer() {
+  bestCustomer = 'not bob';
+  }
+ 
+ console.log(setBestCustomer());
+ 
+  function overwriteBestCustomer(){
+   bestCustomer = 'maybe bob'
+ }
+ console.log(overwriteBestCustomer());
+ const leastFavoriteCustomer = 'Iryna'
+ 
+ function changeLeastFavoriteCustomer(){
+   leastFavoriteCustomer = 'vvv'
+ }
+ console.log(changeLeastFavoriteCustomer());
+ 
+ 
